16b3ce84da8e054fca2613ed15c0dffcfa3c994a
Akinbande Gbenga <olugbengalium@gmail.com>
2023-05-21 01:48:43 +0100

    Initial Commit

---
+++ Helmet/HelmetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelmetApp.swift
+//  Helmet
+//
+//  Created by Gbenga Abayomi on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelmetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
